74ca65bd3324bbf4d7bc968079ac1d40528967a7
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2023-11-07 19:15:21 -0800

    chore: create react app

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <h1>Привет, мир!</h1>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'))
