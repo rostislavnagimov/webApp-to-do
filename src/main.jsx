626328549a82e7d006dd08306fabe0167d425da0
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2023-11-08 14:56:59 -0800

    feat: use other way to connect to telegram

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { useTelegram } from "../TelegramProvider";
 import { Container } from "./components/Container";
 
 import './index.css'
 
 const App = () => {
-  const { webApp } = useTelegram()
-  webApp && webApp.expand()
-
+  window.Telegram.WebApp.expand()
+  
   return (
     <Container />
   );
